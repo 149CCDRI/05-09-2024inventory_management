ca5050df43b819aa61b00c45bf8c155301095a52
Aya Isidro <119942820+4Jded@users.noreply.github.com>
2024-05-09 18:27:29 +0800

    Update models.py

--- inventory_management/inventory/models.py
+++ inventory_management/inventory/models.py
@@ -25,12 +25,12 @@ class Supplier(models.Model):
         return self.name
 
 class Material(models.Model):
-    material_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     initial_quantity = models.IntegerField()
-    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return self.material_name
+        return self.name
 
 class Order(models.Model):
     service = models.ForeignKey(Service, on_delete=models.CASCADE)

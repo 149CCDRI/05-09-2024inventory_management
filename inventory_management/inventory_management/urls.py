8d307a3732784008e47aaa021c53d26b72bfa2c2
xscantoria <xy.cantoria@gmail.com>
2024-05-09 16:25:11 +0800

    add login page

--- inventory_management/inventory_management/urls.py
+++ inventory_management/inventory_management/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from inventory import views
 from user import views as user_view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('inventory/', include('inventory.urls')),
     path('register/', user_view.register, name='user-register'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='user.login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='user.logout'),
 ]
